a3603ac7f9f193d0b4e549990c7007bad64d6d4e
rubystarashe <rubystarashe@gmail.com>
2019-02-08 16:57:11 +0900

    expire reset bug fix

--- plugins/bindStorage/expire.js
+++ plugins/bindStorage/expire.js
@@ -4,7 +4,8 @@ export default {
     let copy = eval('(' + JSON.stringify(val || {}) + ')')
     Object.keys(copy).forEach((key) => {
       try {
-        const expireDate = new Date(copy[key].expire).getTime()
+        const expireDate = new Date(copy[key].___expireDate).getTime()
+        delete copy[key].___expireDate
         if (expireDate < date) delete copy[key]
       } catch (e) {}
     })
@@ -16,7 +17,7 @@ export default {
     Object.keys(copy).forEach((key) => {
       if (typeof copy[key].expire === 'number') {
         const expireDate = date + (copy[key].expire * 60 * 60 * 1000)
-        copy[key].expire = new Date(expireDate)
+        copy[key].___expireDate = new Date(expireDate)
       }
     })
     return copy
